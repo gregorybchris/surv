a2450c001fd823b1f73140ece5ed24d62828f4ed
Chris Gregory <gregorybchris@gmail.com>
2024-08-27 01:14:25 -0700

    Parameterize tests

--- tests/algo/conftest.py
+++ tests/algo/conftest.py
@@ -1,12 +1,33 @@
+import json
+from dataclasses import dataclass
+from enum import StrEnum
 from pathlib import Path
 
 import pytest
 from surv.models.dataset import Dataset
 
 
-@pytest.fixture(scope="session", autouse=True)
-def house_dataset() -> Dataset:
-    data_dirpath = Path(__file__).parent / "data" / "house"
+class DatasetTag(StrEnum):
+    HAS_NUMERIC_TRAINING_FEATURES = "has_numeric_training_features"
+
+
+@dataclass
+class EvaluationDataset:
+    dataset: Dataset
+    tags: set[DatasetTag]
+
+
+def get_evaluation_dataset_names() -> list[str]:
+    return ["house", "latitude"]
+
+
+@pytest.fixture(scope="session", params=get_evaluation_dataset_names())
+def evaluation_dataset(request: pytest.FixtureRequest) -> EvaluationDataset:
+    data_dirpath = Path(__file__).parent / "data" / request.param
     feature_info_filepath = data_dirpath / "features.json"
     tabular_filepath = data_dirpath / "tabular.csv"
-    return Dataset.from_files(tabular_filepath, feature_info_filepath)
+    dataset = Dataset.from_files(tabular_filepath, feature_info_filepath)
+    tags_filepath = data_dirpath / "tags.json"
+    with tags_filepath.open("r") as file:
+        tags = json.load(file)
+    return EvaluationDataset(dataset, tags)
--- tests/algo/test_evaluator.py
+++ tests/algo/test_evaluator.py
@@ -1,14 +1,21 @@
 from typing import TYPE_CHECKING
 
+import pytest
 from surv.algo.evaluator import Evaluator
-from surv.models.dataset import Dataset
+
+from tests.algo.conftest import DatasetTag, EvaluationDataset
 
 if TYPE_CHECKING:
     from surv.algo.constraints import Constraint
 
 
 class TestEvaluator:
-    def test_evaluator(self, house_dataset: Dataset) -> None:
+    def test_evaluator(self, evaluation_dataset: EvaluationDataset) -> None:
+        tags = evaluation_dataset.tags
+        if DatasetTag.HAS_NUMERIC_TRAINING_FEATURES in tags:
+            pytest.skip("Skipping test for dataset with numeric training features.")
+
+        dataset = evaluation_dataset.dataset
         evaluator = Evaluator()
         constraints: list[Constraint] = []
-        evaluator.evaluate(house_dataset, constraints)
+        evaluator.evaluate(dataset, constraints)
